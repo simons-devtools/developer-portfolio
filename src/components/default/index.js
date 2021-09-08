dc1ce216efefc82d6ba76534535cf7bcda143ec1
c-moX <cmoxsh@gmail.com>
2021-09-08 07:46:54 +0600

    added the main

--- src/components/default/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Nav } from './nav';
-export { default as Social } from './social';
-export { default as Email } from './email';
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Head } from './head';
-export { default as Layout } from './layout';
-export { default as Footer } from './footer';
